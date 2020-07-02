53d3b36e351c8f7499c0ab97c8f4235b4142224e
Michael <michael.schubert@edu.campus02.at>
2020-07-02 16:43:44 +0200

    Imported necessary packages to StringQueueTest. Implemented BeforeEach-part for the test.

--- src/test/java/at/fhj/iit/StringQueueTest.java
+++ src/test/java/at/fhj/iit/StringQueueTest.java
@@ -1,12 +1,25 @@
 package at.fhj.iit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Testing StringQueue implementation")
 class StringQueueTest {
 
-   // TODO Write your own tests
+    private StringQueue queue;
 
-}
+    /**
+     * Inits a StringQueue with a max size of 5 for the test.
+     */
+    @BeforeEach
+    void setup() {
+        // SETUP PHASE
+        queue = new StringQueue(5);
+    }
+
+
+    }
